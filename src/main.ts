fcde642dd6d9650ba0efee571772cad1dab602c7
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-04-11 19:35:23 -0300

    Add CSRF Protection with csurf

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 
 import * as helmet from "helmet";
+import * as csurf from 'csurf';
 
 import { AppModule } from './app.module';
 
@@ -8,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
+  app.use(csurf());
 
   await app.listen(3000);
 }
