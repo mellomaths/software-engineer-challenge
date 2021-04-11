b09cb2da3d1f804768ae41a10929bdf2f79a3a9f
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-04-11 20:39:25 -0300

    Add Auth service to validate the Client

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ClientCredentialsService } from './client-credentials.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private clientCredentialsService: ClientCredentialsService) {}
+
+  async validate(id: string, pass: string): Promise<any> {
+    const client = await this.clientCredentialsService.findById(id);
+    if (client && client.secret === pass) {
+      const { secret, ...result } = client;
+      return result;
+    }
+
+    return null;
+  }
+}
