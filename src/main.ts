52045f74e5f5128992f3a6aeeec27e36091de2e6
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-04-11 19:55:42 -0300

    Add Config and TypeORM for database / Remove CSRF Protection to be used later

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 
 import * as helmet from "helmet";
-import * as csurf from 'csurf';
 
 import { AppModule } from './app.module';
 
@@ -9,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
-  app.use(csurf());
 
   await app.listen(3000);
 }
