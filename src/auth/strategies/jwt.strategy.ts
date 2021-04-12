646ab2b19b7ffca2c2b2702a106e44828ca0774e
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-04-11 22:48:37 -0300

    Add JWT Strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('auth.jwt.secret'),
+    });
+  }
+
+  async validate(payload: any) {
+    const { id, email } = payload;
+    return { id, email };
+  }
+}
