9f6ed141e7c3cf8a708e8018efff2827c3be59cb
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-04-13 12:02:32 -0300

    Add GET /users receiving a query param for searching

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+
+export interface GetUsersQuery {
+    search: string;
+}
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get()
+    getUsers(@Query() query: GetUsersQuery) {
+        return query.search
+    }
+}
