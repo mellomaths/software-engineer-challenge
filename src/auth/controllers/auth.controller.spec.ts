53eb6e213f67758a78ade81a7ecc39f849a565d7
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-04-12 23:08:47 -0300

    Change controller tests to pass

--- src/auth/controllers/auth.controller.spec.ts
+++ src/auth/controllers/auth.controller.spec.ts
@@ -1,12 +1,48 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { mockedJwtService } from '../../../test/mocks/jwt.service';
+import { mockedConfigService } from '../../../test/mocks/config.service';
+
+import { ClientEntity } from '../entities/client.entity';
+import { AuthService } from '../services/auth.service';
+import { IdentityService } from '../services/identity.service';
 import { AuthController } from './auth.controller';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
+  let create: jest.Mock;
+  let save: jest.Mock;
+  let findOne: jest.Mock;
+
   beforeEach(async () => {
+    create = jest.fn();
+    save = jest.fn();
+    findOne = jest.fn();
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        IdentityService,
+        {
+          provide: getRepositoryToken(ClientEntity),
+          useValue: {
+            create,
+            save,
+            findOne,
+          },
+        },
+        {
+          provide: ConfigService,
+          useValue: mockedConfigService,
+        },
+        {
+          provide: JwtService,
+          useValue: mockedJwtService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
