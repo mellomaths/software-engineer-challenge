cab4320ee8e7401edbe1f450b411486f02989976
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-04-14 15:58:11 -0300

    Add wait for postgres init in the application docker entrypoint

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -4,6 +4,11 @@ if [ ! -f ".env" ]; then
   cp .env.example .env
 fi
 
+until nc -z ${POSTGRES_HOST} ${POSTGRES_PORT}; do
+  echo "$(date) - waiting for postgres..."
+  sleep 2
+done
+
 npm install
 npm run typeorm migration:run
 npm run start:dev
