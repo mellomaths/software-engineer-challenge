318234b56ae0a43a34bc02be7e6735f1f56a290d
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-04-14 19:50:48 -0300

    Add implementation of find users by keyword

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ServiceResponse } from 'src/utils/service.response';
 import { Like, Repository } from 'typeorm';
 import { UserEntity } from '../entities/user.entity';
-import { UserPriorityEntity } from '../entities/user-priority.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly usersRepository: Repository<UserEntity>,
-    @InjectRepository(UserPriorityEntity)
-    private readonly usersPriorityRepository: Repository<UserPriorityEntity>,
   ) {}
 
-  findUserByKeyword(keyword: string) {
-    return this.usersRepository.find({ where: { fullname: Like(`%${keyword}%`) } });
+  async findUsersByKeyword(keyword: string): Promise<ServiceResponse> {
+    if (!keyword) {
+      return {
+        status: 400,
+        payload: {},
+        errors: [],
+        description: 'A search parameter is required for finding users.',
+      };
+    }
+
+    let users = await this.usersRepository.find({
+      where: [
+        { fullname: Like(`%${keyword}%`) },
+        { username: Like(`%${keyword}%`) },
+      ],
+    });
+
+    users = users.sort((userA, userB) =>
+      userA.getPriorityNumber() > userB.getPriorityNumber() ? 1 : -1,
+    );
+    return { status: 200, payload: { users }, errors: [], description: 'OK' };
   }
 }
