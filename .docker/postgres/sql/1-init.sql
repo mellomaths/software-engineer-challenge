157eb086cf6a83169badd00d2a3cb9b649d6bedc
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-04-14 16:57:40 -0300

    Add Primary Keys and Foreign Keys in the init SQL for the database

--- .docker/postgres/sql/1-init.sql
+++ .docker/postgres/sql/1-init.sql
@@ -3,7 +3,8 @@
 CREATE TABLE Users (
     id VARCHAR(36),
     fullname VARCHAR(50),
-    username VARCHAR(50)
+    username VARCHAR(50),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE Tmp_Users_Priority_One (
@@ -16,7 +17,11 @@ CREATE TABLE Tmp_Users_Priority_Two (
 
 CREATE TABLE Users_Priority (
     user_id VARCHAR(36),
-    priority_num INT
+    priority_num INT,
+    PRIMARY KEY (user_id, priority_num),
+    CONSTRAINT fk_user_id
+      FOREIGN KEY (user_id) 
+	  REFERENCES Users(id)
 );
 
 COMMIT;
