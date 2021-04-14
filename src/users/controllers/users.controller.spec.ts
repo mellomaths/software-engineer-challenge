767d73c5604c8869d41a67359b8ffaf2c53a4514
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-04-14 20:10:08 -0300

    Add tests dependencies for controller and service unittests

--- src/users/controllers/users.controller.spec.ts
+++ src/users/controllers/users.controller.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserPriorityEntity } from '../entities/user-priority.entity';
+import { UserEntity } from '../entities/user.entity';
+import { UsersService } from '../services/users.service';
 import { UsersController } from './users.controller';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let find: jest.Mock;
 
   beforeEach(async () => {
+    find = jest.fn();
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: {
+            find,
+          },
+        },
+        {
+          provide: getRepositoryToken(UserPriorityEntity),
+          useValue: {
+            find,
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
--- src/users/services/users.service.spec.ts
+++ src/users/services/users.service.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserPriorityEntity } from '../entities/user-priority.entity';
+import { UserEntity } from '../entities/user.entity';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  let find: jest.Mock;
+
   beforeEach(async () => {
+    find = jest.fn();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: {
+            find,
+          },
+        },
+        {
+          provide: getRepositoryToken(UserPriorityEntity),
+          useValue: {
+            find,
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
