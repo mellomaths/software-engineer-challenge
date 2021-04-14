685dfa1635d63540caa18e12f74c7599e3ca7e54
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-04-14 20:26:57 -0300

    Imports CacheModule passing customized configuration

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,27 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { UserPriorityEntity } from './entities/user-priority.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import cacheConfig from 'src/config/cache.config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, UserPriorityEntity])],
+  imports: [
+    ConfigModule.forRoot({
+      load: [cacheConfig],
+    }),
+    TypeOrmModule.forFeature([UserEntity, UserPriorityEntity]),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        ttl: configService.get('cache.timeToLive'),
+        max: configService.get('cache.maxResponsesStored'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [UsersService],
   controllers: [UsersController],
 })
