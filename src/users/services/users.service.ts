a31684d4b9c4524afa7ef08c4182a15035b9e9a7
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-04-18 16:39:20 -0300

    Add logger to users service

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,4 @@
-import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable, Logger } from '@nestjs/common';
 import { Cache } from 'cache-manager';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ServiceResponse } from 'src/utils/service.response';
@@ -13,6 +13,9 @@ export interface FindUsersQuery {
 
 @Injectable()
 export class UsersService {
+
+  private readonly logger = new Logger(UsersService.name);
+
   constructor(
     @InjectRepository(UserEntity)
     private readonly usersRepository: Repository<UserEntity>,
@@ -40,13 +43,20 @@ export class UsersService {
       start: parseInt(start.toString()),
       limit: parseInt(limit.toString()),
     };
+    this.logger.log(`findUsers:: Searching for users using parameters: ${JSON.stringify(query)}`);
+    this.logger.log(`findUsers:: Checking if the payload is saved in cache.`);
 
     const cacheKey = this.cacheKey.of.findUsers(query);
+    this.logger.log(`findUsers:: Cache Key: ${cacheKey}`);
     let users: UserEntity[] = await this.cacheManager.get(cacheKey);
     if (users) {
+      this.logger.log(`findUsers:: Cache hit.`);
+      this.logger.log(`findUsers:: Users found using cache.`);
       return { status: 206, payload: { users }, errors: [], description: 'OK' };
     }
 
+    this.logger.log(`findUsers:: Users searched was not found at cache, searching now the database.`);
+    this.logger.log(`findUsers:: Building query for the database.`);
     const options: FindManyOptions = {
       take: query.limit,
       skip: query.start,
@@ -59,13 +69,21 @@ export class UsersService {
       ];
     }
 
-    users = await this.usersRepository.find(options);
+    this.logger.log(`findUsers:: Query built: ${JSON.stringify(options)}`);
 
+    users = await this.usersRepository.find(options);
+    this.logger.log(`findUsers:: Count of users returned from the database: ${users.length}.`);
+
+    this.logger.log(`findUsers:: Sorting the users by priority.`);
     users = users.sort((userA, userB) =>
       userA.getPriorityNumber() > userB.getPriorityNumber() ? 1 : -1,
     );
 
+    this.logger.log(`findUsers:: Users sorted.`);
+    this.logger.log(`findUsers:: Saving users at cache.`);
+    this.logger.log(`findUsers:: Cache Key: ${cacheKey}`);
     this.cacheManager.set(cacheKey, users);
+    this.logger.log(`findUsers:: Returning users partial content from the database.`);
     return {
       status: 206,
       payload: {
