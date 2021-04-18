7f6989e3e40cfa6511dcce28dec7b62958cceedf
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-04-18 17:46:30 -0300

    Add mock for user service tests

--- src/application/users/services/users.service.spec.ts
+++ src/application/users/services/users.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { RedisService } from '../../../infrastructure/redis/services/redis/redis.service';
 import { UserPriorityEntity } from '../entities/user-priority.entity';
 import { UserEntity } from '../entities/user.entity';
 import { UsersService } from './users.service';
@@ -8,9 +9,13 @@ describe('UsersService', () => {
   let service: UsersService;
 
   let find: jest.Mock;
+  let get: jest.Mock;
+  let set: jest.Mock;
 
   beforeEach(async () => {
     find = jest.fn();
+    get = jest.fn();
+    set = jest.fn();
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
@@ -24,6 +29,13 @@ describe('UsersService', () => {
           provide: getRepositoryToken(UserPriorityEntity),
           useValue: {
             find,
+          },
+        },
+        {
+          provide: RedisService,
+          useValue: {
+            get,
+            set
           },
         },
       ],
