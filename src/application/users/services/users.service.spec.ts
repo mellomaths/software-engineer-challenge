3f2fa49af77d0f6ce8bc0e48b5d0f7f36dac0205
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-04-18 18:31:54 -0300

    Add find all users from database unittest

--- src/application/users/services/users.service.spec.ts
+++ src/application/users/services/users.service.spec.ts
@@ -5,6 +5,12 @@ import { UserPriorityEntity } from '../entities/user-priority.entity';
 import { UserEntity } from '../entities/user.entity';
 import { UsersService } from './users.service';
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 describe('UsersService', () => {
   let service: UsersService;
 
@@ -47,4 +53,48 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('findUsers', () => {
+    let users: UserEntity[];
+    let usersCount: number;
+
+    beforeEach(() => {
+      usersCount = 20;
+      users = [];
+      for (let i = 0; i < usersCount; i++) {
+        const user = new UserEntity();
+        user.id = String(i);
+        user.fullname = 'Test ' + new Date().getTime();
+        user.username = 'test' + new Date().getTime() + '@email.com';
+        user.priority = null;
+        const priority = getRandomIntInclusive(1, 3);
+        if (priority < 3) {
+          user.priority = new UserPriorityEntity();
+          user.priority.user_id = String(i);
+          user.priority.priority_num = priority;
+        }
+        users.push(user);
+      }
+      
+      find.mockReturnValue(users);
+      set.mockReturnValue(null);
+    });
+
+    it('should find all users from database without searching by a keyword', async () => {
+      get.mockReturnValue(null);
+      const query = { search: '', start: 0, limit: usersCount+1 };
+      const serviceResponse = await service.findUsers(query);
+      expect(serviceResponse.status).toEqual(206);
+      expect(serviceResponse.errors.length).toEqual(0);
+      expect(serviceResponse.description).toEqual('OK');
+      expect(serviceResponse.payload).toBeDefined();
+      expect(serviceResponse.payload.pagination).toBeDefined();
+      expect(serviceResponse.payload.pagination.start).toEqual(query.start);
+      expect(serviceResponse.payload.pagination.limit).toEqual(query.limit);
+      expect(serviceResponse.payload.pagination.count).toEqual(usersCount);
+      expect(serviceResponse.payload.result).toBeDefined();
+      expect(serviceResponse.payload.result.length).toEqual(usersCount);
+    });
+  });
+
 });
