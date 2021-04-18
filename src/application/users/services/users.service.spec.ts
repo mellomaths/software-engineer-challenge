c8de023fcf8d6211efbe5fb97dfc175ff36d3034
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-04-18 19:40:41 -0300

    Insert static users to mock database and fix the search for keyword test

--- src/application/users/services/users.service.spec.ts
+++ src/application/users/services/users.service.spec.ts
@@ -5,12 +5,6 @@ import { UserPriorityEntity } from '../entities/user-priority.entity';
 import { UserEntity } from '../entities/user.entity';
 import { UsersService } from './users.service';
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 describe('UsersService', () => {
   let service: UsersService;
 
@@ -83,7 +77,9 @@ describe('UsersService', () => {
         }
         users.push(user);
       }
-      
+
+      users = insertStaticUsers(users, 2);
+      usersCount += 2;
       queryBuilder.getMany.mockReturnValue(users);
       set.mockReturnValue(null);
     });
@@ -101,24 +97,55 @@ describe('UsersService', () => {
       expect(serviceResponse.payload.pagination.count).toEqual(usersCount);
       expect(serviceResponse.payload.result).toBeDefined();
       expect(serviceResponse.payload.result.length).toEqual(usersCount);
+      
+      const usersFound = serviceResponse.payload.result;
+      expect(usersFound[0].priority).toBeDefined();
+      expect(usersFound[0].priority).toBeDefined();
     });
 
     it('should find users by searching a keyword', async () => {
       queryBuilder.getMany.mockReturnValue(users.slice(0, 2));
       get.mockReturnValue(null);
-      const query = { search: 'test', start: 0, limit: 2 };
+      const query = { search: 'dev' };
       const serviceResponse = await service.findUsers(query);
       expect(serviceResponse.status).toEqual(206);
       expect(serviceResponse.errors.length).toEqual(0);
       expect(serviceResponse.description).toEqual('OK');
       expect(serviceResponse.payload).toBeDefined();
       expect(serviceResponse.payload.pagination).toBeDefined();
-      expect(serviceResponse.payload.pagination.start).toEqual(query.start);
-      expect(serviceResponse.payload.pagination.limit).toEqual(query.limit);
-      expect(serviceResponse.payload.pagination.count).toEqual(query.limit);
+      expect(serviceResponse.payload.pagination.start).toEqual(0);
+      expect(serviceResponse.payload.pagination.limit).toEqual(100);
+      expect(serviceResponse.payload.pagination.count).toEqual(2);
       expect(serviceResponse.payload.result).toBeDefined();
-      expect(serviceResponse.payload.result.length).toEqual(query.limit);
+      expect(serviceResponse.payload.result.length).toEqual(2);
     });
   });
 
 });
+
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function insertStaticUsers(users: UserEntity[], howMany: number): UserEntity[] {
+  const user1 = new UserEntity();
+  user1.id = getRandomIntInclusive(10000, 100000);
+  user1.fullname = 'Dev Software Engineer';
+  user1.username = 'dev.software.engineer';
+  user1.priority = new UserPriorityEntity();
+  user1.priority.priority_num = 1;
+  user1.priority.user_id = user1.id;
+  users.push(user1);
+  const user2 = new UserEntity();
+  user2.id = getRandomIntInclusive(10000, 100000);
+  user2.fullname = 'Dev Data Scientist';
+  user2.username = 'dev.data.scientist';
+  user2.priority = new UserPriorityEntity();
+  user2.priority.priority_num = 2;
+  user2.priority.user_id = user2.id;
+  users.push(user2);
+  return users;
+}
